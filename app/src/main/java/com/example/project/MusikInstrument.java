610fe7cabf8c87b10d99c4d67d2b3c209b6ba1cd
a23gabst <a23gabst@student.his.se>
2024-05-02 18:36:21 +0200

    Lagt till getters, setters och konstruktorer för MusikInstrument klassen

--- app/src/main/java/com/example/project/MusikInstrument.java
+++ app/src/main/java/com/example/project/MusikInstrument.java
@@ -11,4 +11,61 @@ public class MusikInstrument {
     @SerializedName("category")
     private String genre;
     private int cost;
+
+    //Standard konstruktor
+    public MusikInstrument () {
+        id="Saknar ID";
+        login="Login saknas";
+        name="Namn saknas";
+        genre="Saknar genre";
+        cost=1;
+    }
+
+    public MusikInstrument(String id, String login, String name, String genre, int cost) {
+        this.id = id;
+        this.login = login;
+        this.name = name;
+        this.genre = genre;
+        this.cost = cost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
