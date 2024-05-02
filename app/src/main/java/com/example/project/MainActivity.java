99229c603ae1615ab3126aa40bb24164655d64d9
a23gabst <a23gabst@student.his.se>
2024-05-02 21:08:15 +0200

    Skapat en privat medlemsvariabel som lagrar JSON-data och en onPostExecute() i MainActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener{
+    private final String JSON_URL="https://mobprog.webug.se/json-api?login=a23gabst";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
     }
 }
