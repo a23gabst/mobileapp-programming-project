aaf4d43c09e6fe8489050d7f43d38c5fbaab4488
a23gabst <a23gabst@student.his.se>
2024-05-03 09:09:51 +0200

    Lagt till en ny privat ArrayList, recyclerViewItems i for-loopen och en adapter i MainActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL="https://mobprog.webug.se/json-api?login=a23gabst";
 
     private ArrayList<MusikInstrument> instruments= new ArrayList<>();
+    private ArrayList<RecyclerViewItem> recyclerViewItems=new ArrayList<>();
+    private RecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +30,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
         for (int i=0;i<instruments.size();i++) {
             Log.d("Injera", instruments.get(i).toString());
+            recyclerViewItems.add(new RecyclerViewItem(instruments.get(i).toString()));
         }
+
+        adapter=new RecyclerViewAdapter(this, recyclerViewItems, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         new JsonTask(this).execute(JSON_URL);
     }
